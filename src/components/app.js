cb6a3ba68548028b95963a7b0b33ea569cba37d3
paschalidi <paschalidis@factor-e.eu>
2016-10-20 00:24:46 +0300

    master: Adds a main files and file structure

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>ReactJS</h1>
+                <h2>react-starter-pack</h2>
+                <h3>Clone from: https://github.com/paschalidi/react-starter-pack</h3>
+            </div>
+        );
+    }
+}
