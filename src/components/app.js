2c551acbc7d592faf7b21c7a59c5490f62b1bfaf
paschalidi <paschalidis@factor-e.eu>
2016-10-20 13:11:59 +0300

    10_egghead_react_tutorial: learns update Lifecycle methods of a React components

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,50 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 export default class App extends React.Component {
+    constructor() {
+        super();
+        this.update = this.update.bind(this);
+    }
+
+    update() {
+        ReactDOM.render(
+            <App val={this.props.val + 1}/>, document.getElementById('app'))
+    }
+
+    componentWillReceiveProps(nextProps) {
+        console.log('componentWillReceiveProps');
+        console.log('nextProps', nextProps);
+        console.log('this.props.val', this.props.val);
+        console.log('-');
+        this.setState({increasing: nextProps.val > this.props.val})
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.val % 5 === 0
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log('componentDidUpdate');
+        console.log('prevProps', prevProps);
+        console.log('prevState', prevState);
+        console.log('---')
+    }
+
+
     render() {
+        console.log('render');
+        console.log('this.state', this.state);
+        console.log('this.state', this.props);
+        console.log('--');
         return (
             <div>
-                <h1>ReactJS</h1>
-                <h2>react-starter-pack</h2>
-                <h3>Clone from: https://github.com/paschalidi/react-starter-pack</h3>
+                <button onClick={this.update}>
+                    {this.props.val}
+                </button>
             </div>
         );
     }
 }
+
+App.defaultProps = {val: 0}
