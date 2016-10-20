40cdb993b9389f3438504524080c92eaaf80a5e9
paschalidi <paschalidis@factor-e.eu>
2016-10-20 11:31:30 +0300

    09_egghead_react_tutorial: Understand the React Component Life cycle Methods

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,70 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
-export default class App extends React.Component {
+class App extends React.Component {
     render() {
         return (
             <div>
-                <h1>ReactJS</h1>
-                <h2>react-starter-pack</h2>
-                <h3>Clone from: https://github.com/paschalidi/react-starter-pack</h3>
+                <Button>React Button</Button>
             </div>
         );
+    }
+}
+
+class Button extends React.Component {
+    constructor() {
+        super();
+        this.state = {val: 0};
+        this.update = this.update.bind(this)
+    }
+
+    update() {
+        this.setState({val: this.state.val + 1})
+    }
+
+    componentWillMount() {
+        console.log('will')
+    }
+
+    componentDidMount() {
+        console.log('did')
+    }
+
+    componentWillUnmount() {
+        console.log('bye')
+    }
+
+    render() {
+        console.log('rendering')
+        return (
+            <div>
+                <button onClick={this.update}>{this.state.val}</button>
+                <div > {this.props.state}</div>
+            </div>
+        );
+    }
+}
+
+export default class Wrapper extends React.Component {
+    constructor() {
+        super();
+    }
+
+    mount() {
+        ReactDOM.render(<App/>, document.getElementById('a'))
+    }
+
+    unmount() {
+        ReactDOM.unmountComponentAtNode(document.getElementById('a'))
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.mount.bind(this)}>Mount</button>
+                <button onClick={this.unmount.bind(this)}>Unmount</button>
+                <div id='a'></div>
+            </div>
+        )
     }
 }
